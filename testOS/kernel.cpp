29f50329c89002f50a683e44bfdc2119ecd6b541
herozxb <herozxb@gmail.com>
2018-03-08 01:42:36 -0800

    add the kernel file

--- testOS/kernel.cpp
+++ testOS/kernel.cpp
@@ -1,47 +1,33 @@
 #include "./include/types.h"
-
-void printf(char* str)
-{
-    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
-
-    static uint8_t x=0,y=0;
-    
-    for(int i = 0; str[i] != '\0'; ++i)
-    {
-        switch(str[i])
-        {
-            case '\n':
-                x = 0;
-                y++;
-                break;
-            default:
-                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
-                x++;
-                break;
-        }
-        
-        if(x >= 80)
-        {
-            x = 0;
-            y++;
-        }
-        
-        if(y >= 25)
-        {
-            for(y = 0; y < 25; y++)
-                for(x = 0; x < 80; x++)
-                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
-            x = 0;
-            y = 0;
-        }
-    }
-}
-
+#include "./include/print.h"
+#include "./include/port.h"
+#include "./include/globalDescriptorTable.h"
+#include "./include/interrupts.h"
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t multiboot_magic)
 {
-    printf("Hello World! Write my first OS by hand from ZXB");
+    	printf("Hello World! Write my first OS by hand from ZXB\n");
+	////////////////////////////////////////////////////////////////////////////////////
+	//setup the Table
+    	GlobalDescriptorTable gdt;
+	setupSegmentDescriptor((uint8_t*) &(gdt.nullSegmentSelector),0,0,0);
+	setupSegmentDescriptor((uint8_t*) &(gdt.unusedSegmentSelector),0,0,0);
+	setupSegmentDescriptor((uint8_t*) &(gdt.codeSegmentSelector),0,64*1024*1024,0x9A);
+	setupSegmentDescriptor((uint8_t*) &(gdt.dataSegmentSelector),0,64*1024*1024,0x92);
+	loadTable(&gdt);
+	dataSegmentSelector(&gdt);
+	codeSegmentSelector(&gdt);
+	Base(&gdt);
+	Limit(&gdt);
+	printf("here now 1\n");
+	//
+	////////////////////////////////////////////////////////////////////////////////////
+	printf("before here now 2\n");
+	uint16_t hardwareInterruptOffset_in = 0x20;
+	static GateDescriptor interruptDescriptorTable[256];
+	interruptManager(hardwareInterruptOffset_in, &gdt,interruptDescriptorTable);
+	printf("here now 2\n");
 
-    while(1);
+    	while(1);
 }
 
