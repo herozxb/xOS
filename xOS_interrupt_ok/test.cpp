7a90ddfee4c2aaccc30e017db5e42e27dd52ea16
herozxb <herozxb@gmail.com>
2018-03-13 00:44:34 -0700

    the interrupt function is done

---
+++ xOS_interrupt_ok/test.cpp
@@ -0,0 +1,21 @@
+#include "test.h"
+
+//void HandleException0x9F(){}
+
+void test()
+{
+	printf("at the test function 0 0 0 it's ok, hello world!\n");
+}
+
+void test1()
+{
+	printf("at the test function 1 1 1 it's ok, hello world!\n");
+}
+
+
+
+void test2()
+{
+	printf(" 2 2 2 why ??? it's ok, hello world!\n");
+}
+//*/
