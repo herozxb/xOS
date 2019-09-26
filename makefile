9d3a6c285f31e7f4b3bb082fccf6d2746e3e65a0
herozxb <herozxb@gmail.com>
2019-09-26 15:50:28 +0800

    windows

--- makefile
+++ makefile
@@ -15,6 +15,10 @@ objects = obj/loader.o \
           obj/hardwarecommunication/pci.o \
           obj/drivers/keyboard.o \
           obj/drivers/mouse.o \
+          obj/drivers/vga.o \
+          obj/gui/widget.o \
+          obj/gui/window.o \
+          obj/gui/desktop.o \
           obj/kernel.o
 
 
--- src/kernel.cpp
+++ src/kernel.cpp
@@ -6,11 +6,19 @@
 #include <drivers/driver.h>
 #include <drivers/keyboard.h>
 #include <drivers/mouse.h>
+#include <drivers/vga.h>
+#include <gui/desktop.h>
+#include <gui/window.h>
+
+
+#define GRAPHICSMODE 1
+
 
 using namespace myos;
 using namespace myos::common;
 using namespace myos::drivers;
 using namespace myos::hardwarecommunication;
+using namespace myos::gui;
 
 
 
@@ -131,31 +139,61 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/)
 {
-    printf("Hello World!\n");
+    printf("Hello World! --- http://www.AlgorithMan.de\n");
 
     GlobalDescriptorTable gdt;
     InterruptManager interrupts(0x20, &gdt);
     
     printf("Initializing Hardware, Stage 1\n");
     
+    #ifdef GRAPHICSMODE
+        Desktop desktop(320,200, 0x00,0x00,0xA8);
+    #endif
+    
     DriverManager drvManager;
     
-        PrintfKeyboardEventHandler kbhandler;
-        KeyboardDriver keyboard(&interrupts, &kbhandler);
+        #ifdef GRAPHICSMODE
+            KeyboardDriver keyboard(&interrupts, &desktop);
+        #else
+            PrintfKeyboardEventHandler kbhandler;
+            KeyboardDriver keyboard(&interrupts, &kbhandler);
+        #endif
         drvManager.AddDriver(&keyboard);
+        
     
-        MouseToConsole mousehandler;
-        MouseDriver mouse(&interrupts, &mousehandler);
+        #ifdef GRAPHICSMODE
+            MouseDriver mouse(&interrupts, &desktop);
+        #else
+            MouseToConsole mousehandler;
+            MouseDriver mouse(&interrupts, &mousehandler);
+        #endif
         drvManager.AddDriver(&mouse);
         
         PeripheralComponentInterconnectController PCIController;
-        PCIController.SelectDrivers(&drvManager);
+        PCIController.SelectDrivers(&drvManager, &interrupts);
 
+        VideoGraphicsArray vga;
+        
     printf("Initializing Hardware, Stage 2\n");
         drvManager.ActivateAll();
         
     printf("Initializing Hardware, Stage 3\n");
-    interrupts.Activate();
 
-    while(1);
+    #ifdef GRAPHICSMODE
+        vga.SetMode(320,200,8);
+        Window win1(&desktop, 10,10,20,20, 0xA8,0x00,0x00);
+        desktop.AddChild(&win1);
+        Window win2(&desktop, 40,15,30,30, 0x00,0xA8,0x00);
+        desktop.AddChild(&win2);
+    #endif
+
+
+    interrupts.Activate();
+    
+    while(1)
+    {
+        #ifdef GRAPHICSMODE
+            desktop.Draw(&vga);
+        #endif
+    }
 }
