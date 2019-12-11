863db90389856c7b2a6ece61ff8d9fd4cbee2bd3
herozxb <herozxb@gmail.com>
2019-12-11 17:46:46 +0800

    correct new function

--- bootloader_key_functions/boot.asm
+++ bootloader_key_functions/boot.asm
@@ -1,19 +1,80 @@
-org 0x7C00                      ; BIOS loads our programm at this address
-bits 16                         ; We're working at 16-bit mode here
+[org 0x7C00]
+[bits 16]
 
-start:
-	cli                     ; Disable the interrupts
-	mov si, msg             ; SI now points to our message
-	mov ah, 0x0E            ; Indicate BIOS we're going to print chars
-.loop	lodsb                   ; Loads SI into AL and increments SI [next char]
-	or al, al               ; Checks if the end of the string
-	jz halt                 ; Jump to halt if the end
-	int 0x10                ; Otherwise, call interrupt for printing the char
-	jmp .loop               ; Next iteration of the loop
+KERNEL_OFFSET equ 0x1000
 
-halt:	hlt                     ; CPU command to halt the execution
-msg:	db "Hello, World!", 0   ; Our actual message to print
+mov [BOOT_DRIVE], dl
+call load_boot
+call execute_boot
 
-;; Magic numbers
+load_boot:
+	pusha
+	mov bx, LOADING_BOOT_MSG
+	call print_string
+	mov bx, KERNEL_OFFSET
+	mov dh, 15
+	mov dl, [BOOT_DRIVE]
+	call disk_read
+	popa
+	ret
+
+execute_boot:
+	mov bx, EXECUTING_BOOT_MSG
+	call print_string
+	call KERNEL_OFFSET
+	jmp $
+
+disk_read:
+	pusha
+	push dx
+
+	mov ah, 0x02
+	mov al, dh
+	mov ch, 0x00
+	mov dh, 0x00
+	mov cl, 0x02
+	int 0x13
+
+	jc disk_read_error
+
+	pop dx
+	cmp dh, al
+	jne disk_read_error
+
+	popa
+	ret
+
+disk_read_error:
+	mov bx, DISK_READ_ERROR_MSG
+	call print_string
+	hlt
+
+print_string:
+	pusha
+
+	.loop:
+	mov al, [bx]
+	cmp al, 0
+	je .ret
+	mov ah, 0x0E
+	int 0x10
+	inc bx
+	jmp .loop
+
+	.ret:
+	mov ah, 0x0E
+	mov al, 0x0A
+	int 0x10
+	mov al, 0x0D
+	int 0x10
+
+	popa
+	ret
+
+BOOT_DRIVE: db 0
+LOADING_BOOT_MSG: db "Loading Boot", 0
+EXECUTING_BOOT_MSG: db "Executing Boot", 0
+DISK_READ_ERROR_MSG: db "Disk Error", 0
+
 times 510 - ($ - $$) db 0
 dw 0xAA55
