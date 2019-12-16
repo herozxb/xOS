5e4fc16ee460f3094630a51f28e8c83604d4f185
herozxb <herozxb@gmail.com>
2019-12-16 16:27:54 +0800

    first kernel movement to self made enviroment success

--- a_kernel_-Ttext_0x100000_for_org/instruction.md
+++ a_kernel_-Ttext_0x100000_for_org/instruction.md
@@ -17,3 +17,8 @@ qemu-system-i386 -fda floppy.img
 gcc -ffreestanding -m32 -c kernel.c -o kernel.o
 nasm kernel_entry.asm -f elf -o kernel_entry.o
 ld -o KRNL.SYS  -m elf_i386 -Ttext 0x100000 kernel_entry.o kernel.o --oformat binary
+
+# add super level function
+gcc -ffreestanding -m32 -c DebugDisplay.c -o DebugDisplay.o
+gcc -ffreestanding -m32 -c string.c -o string.o
+ld -o KRNL.SYS  -m elf_i386 -Ttext 0x100000 kernel_entry.o kernel.o DebugDisplay.o string.o --oformat binary
--- a_kernel_-Ttext_0x100000_for_org/kernel.c
+++ a_kernel_-Ttext_0x100000_for_org/kernel.c
@@ -1,9 +1,53 @@
 /* This will force us to create a kernel entry function instead of jumping to kernel.c:0x00 */
+
+
+#include "DebugDisplay.h"
+
+
 void dummy_test_entrypoint() {
     char* video_memory = (char*) 0xb8000;
     *video_memory = 'A';
 }
 
+
+void dummy_test_entrypoint2() {
+	for (int i = 0; i < 26; i++) {
+		char c = 0x41 + i;
+
+		asm(
+			"mov %0, %%al;"
+			"mov $0x0E, %%ah;"
+			"int $0x10;"
+			:
+			: "r" (c)
+		);
+	}
+}
+//*/
+
 void main() {
-    dummy_test_entrypoint();
+  
+	int i=0x12;
+
+	DebugClrScr (0x18);
+
+	DebugGotoXY (4,4);
+	DebugSetColor (0x17);
+	DebugPrintf ("+-----------------------------------------+\n");
+	DebugPrintf ("|    MOS 32 Bit C++ Kernel Executing!     |\n");
+	DebugPrintf ("+-----------------------------------------+\n\n");
+	DebugSetColor (0x12);
+
+	DebugSetColor (0x12);
+	DebugPrintf ("\ni as integer ........................");
+	DebugPrintf ("\ni in hex ............................");
+
+	DebugGotoXY (25,8);
+	DebugSetColor (0x1F);
+	DebugPrintf ("\n[%i]",i);
+	DebugPrintf ("\n[0x%x]",i);
+
+	DebugGotoXY (4,16);
+	DebugSetColor (0x1F);
+	DebugPrintf ("\n\nI am preparing to load... Hold on, please... :)");
 }
